4b0c18c37c6e74d84632b4a2de348c1dc853374e
Yin Yin Wu <yinnywu@gmail.com>
2016-08-15 12:58:02 -0700

    redis

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+var Redis = require('ioredis');
+var redis = new Redis(6379, '192.241.129.187');
 
 app.get('/', function (req, res) {
-  res.send('Hello World, ' + process.env['person_name']);
+  var person_name;
+  redis.get('person_name', function (err, result) {
+      person_name = result;
+      res.send('Hello World, ' + person_name);
+  });
 });
 
 app.listen(3000, function () {
